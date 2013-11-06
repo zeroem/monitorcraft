5d4777aa0555e34c9b87478c2224b21371b43bdb
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-06 01:01:05 -0800

    get a copy of the minecraft server jar

--- get-deps.sh
+++ get-deps.sh
@@ -4,3 +4,5 @@ git clone https://github.com/eggsby/ansible-supervisor /tmp/ansible-supervisor
 
 cp -r /tmp/ansible-supervisor/supervise ./roles
 cp -r /tmp/ansible-supervisor/supervisord ./roles
+
+wget -q -P ./roles/minecraft/files https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/minecraft_server.1.7.2.jar
