a68fa4feb702bd59c23b7ddc696ae9d2f7ead37f
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-06 11:59:45 -0800

    drop supervisor in favor of upstart

--- get-deps.sh
+++ get-deps.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-git clone https://github.com/eggsby/ansible-supervisor /tmp/ansible-supervisor
-
-cp -r /tmp/ansible-supervisor/supervise ./roles
-cp -r /tmp/ansible-supervisor/supervisord ./roles
-
 wget -q -P ./roles/minecraft/files https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/minecraft_server.1.7.2.jar
