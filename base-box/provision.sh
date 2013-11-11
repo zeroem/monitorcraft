5383f15c05ffaf68f392701e4ca3d9f94bad1648
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-10 23:05:14 -0800

    added graphite and statsd system dependencies

--- base-box/provision.sh
+++ base-box/provision.sh
@@ -3,7 +3,12 @@
 set -e
 
 apt-get update
-apt-get -y --fix-missing install python-pip python-virtualenv python-apt python-pycurl python-dev python-setuptools python-setuptools-git python-pkg-resources git openjdk-7-jre-headless
+apt-get -y --fix-missing install \
+  python-pip python-virtualenv python-apt python-pycurl python-dev \
+  python-setuptools python-setuptools-git python-pkg-resources git \
+  openjdk-7-jre-headless libcairo2-dev python-cairo python-rrdtool \
+  memcached nodejs
+
 apt-get -y remove --auto-remove puppet 
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
