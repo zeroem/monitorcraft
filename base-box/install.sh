d10ccde817da014b12cdac8ae119f1aecd1bbf7c
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-12 10:18:04 -0800

    Hack to fix the broken lxc-template in pre-0.7.0 packaged boxes

--- base-box/install.sh
+++ base-box/install.sh
@@ -3,4 +3,8 @@
 vagrant up --provider lxc
 vagrant package
 vagrant box add ubuntu13-monitorcraft-base package.box --force --provider lxc 
+
+# Hack to deal with using a pre 0.7.0 box with a 0.7.0+ box
+cp ~/.vagrant.d/gems/gems/vagrant-lxc*/boxes/common/lxc-template ~/.vagrant.d/boxes/ubuntu13-monitorcraft-base/lxc/
+
 #vagrant destroy --force
