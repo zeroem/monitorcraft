217a02bb26a4425a2098fd4d0d4aa43de80418a9
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-19 00:39:50 -0800

    basic collectd configuration

--- base-box/provision.sh
+++ base-box/provision.sh
@@ -7,7 +7,7 @@ apt-get -y --fix-missing install \
   python-pip python-virtualenv python-apt python-pycurl python-dev \
   python-setuptools python-setuptools-git python-pkg-resources git \
   openjdk-7-jre-headless libcairo2-dev python-cairo python-rrdtool \
-  memcached nodejs
+  memcached nodejs collectd
 
 apt-get -y remove --auto-remove puppet 
 
