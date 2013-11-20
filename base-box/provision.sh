9d658f7c1dbef02d0dad06552bc8198832244110
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-20 11:39:32 -0800

    extracted graphite deps from the base to the graphite role

--- base-box/provision.sh
+++ base-box/provision.sh
@@ -6,8 +6,7 @@ apt-get update
 apt-get -y --fix-missing install \
   python-pip python-virtualenv python-apt python-pycurl python-dev \
   python-setuptools python-setuptools-git python-pkg-resources git \
-  openjdk-7-jre-headless libcairo2-dev python-cairo python-rrdtool \
-  memcached nodejs collectd
+  openjdk-7-jre-headless collectd
 
 dpkg -i /vagrant/riemann_0.2.3_all.deb
 
