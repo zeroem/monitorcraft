324737e477f382aa7a11401635e8a759bb30328c
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-26 15:36:57 -0800

    riemann is the most resilient to unavailable services, so start it first

--- launch.sh
+++ launch.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+vagrant up riemann
+
 # These boxes recieve data from other boxes and should be
 # running before anything else
-vagrant up redis
-vagrant up graphite
-vagrant up riemann
+vagrant up redis graphite
 
 # start up everything else
 vagrant up
