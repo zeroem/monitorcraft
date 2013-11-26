b551b981661a5de436b2e7c6188a77ac6feaf829
Darrell Hamilton <darrell.noice@gmail.com>
2013-11-26 15:35:51 -0800

    remove riemann from the base box

--- base-box/provision.sh
+++ base-box/provision.sh
@@ -8,10 +8,7 @@ apt-get -y --fix-missing install \
   python-setuptools python-setuptools-git python-pkg-resources git \
   openjdk-7-jre-headless openjdk-7-jdk collectd
 
-dpkg -i /vagrant/riemann_0.2.4_all.deb
-
 apt-get -y remove --auto-remove puppet
-
 
 # clean up collectd data
 service collectd stop
@@ -19,5 +16,6 @@ rm -rf /var/lib/collectd/rrd
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
+# work around for the vagrant-lxc boxes not having appropriate sudoers permissions for vagrant
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
