c81be071fa5f5ddea0d13b2acffa228e00d28f0b
Darrell Hamilton <darrell.noice@gmail.com>
2013-12-12 14:40:33 -0800

    update to minecraft server 1.7.4

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -e
 
 ROLE_DIR=./ansible/roles
 
-wget -q -P $ROLE_DIR/minecraft/files https://s3.amazonaws.com/Minecraft.Download/versions/1.7.2/minecraft_server.1.7.2.jar &
+wget -q -P $ROLE_DIR/minecraft/files https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar &
 wget -q -P $ROLE_DIR/logstash/files https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar &
 wget -q -P $ROLE_DIR/redis/files wget http://download.redis.io/releases/redis-2.8.0.tar.gz &
 wget -q -P $ROLE_DIR/riemann/files http://aphyr.com/riemann/riemann_0.2.4_all.deb &
