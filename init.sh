deba8b2c8ce72b92a571cc5a6f4c92f4bdeab1a9
Darrell Hamilton <darrell.noice@gmail.com>
2013-12-30 20:33:51 -0800

    bump redis and logstash versions

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ set -e
 ROLE_DIR=./ansible/roles
 
 wget -q -P $ROLE_DIR/minecraft/files https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar &
-wget -q -P $ROLE_DIR/logstash/files https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar &
-wget -q -P $ROLE_DIR/redis/files wget http://download.redis.io/releases/redis-2.8.0.tar.gz &
+wget -q -P $ROLE_DIR/logstash/files https://download.elasticsearch.org/logstash/logstash/logstash-1.3.2-flatjar.jar &
+wget -q -P $ROLE_DIR/redis/files wget http://download.redis.io/releases/redis-2.8.3.tar.gz &
 wget -q -P $ROLE_DIR/riemann/files http://aphyr.com/riemann/riemann_0.2.4_all.deb &
 
 cd base-box
